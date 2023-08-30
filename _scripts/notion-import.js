3b6269d3202b954ba9f120250169f13927829e9a
tomgiagtz <tom.giagtzoglou@gmail.com>
2023-08-30 17:06:26 -0400

    sanity check

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -17,7 +17,8 @@ const { VideoTransformer, ImageTransformer } = require('./transformers');
 const envPath = path.resolve(__dirname, '../.env');
 require('dotenv').config({ path: envPath });
 
-console.log('Testing ENV, Database ID: ', process.env.DATABASE_ID);
+console.log('Testing ENV, Database ID check: ', process.env.DATABASE_ID);
+console.log('Sanity Check:', 'hiya' == process.env.TEST);
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
